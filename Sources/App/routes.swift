46b746699d38993a7979e053ab461347d1e7a44d
caike <caike@djcars.cn>
2018-06-12 10:08:38 +0800

    first commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,13 +3,37 @@ import Vapor
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     // Basic "Hello, world!" example
-    router.get("hello") { req in
+    router.get("hello","vapor") { req in
         return "Hello, world!"
     }
-
+    
+    router.get("hello", String.parameter) { req -> String in
+        
+        let name = try req.parameters.next(String.self)
+        return "Hello, \(name)!"
+    }
+    
+    router.post(InfoData.self, at: "info") { (req, data) -> InfoResponse in
+        return InfoResponse(data: data)
+        
+    }
+    
     // Example of configuring a controller
     let todoController = TodoController()
     router.get("todos", use: todoController.index)
     router.post("todos", use: todoController.create)
     router.delete("todos", Todo.parameter, use: todoController.delete)
 }
+
+struct InfoData: Content {
+    var name:String
+    
+}
+
+struct InfoResponse: Content {
+    let data: InfoData
+}
+
+
+
+
